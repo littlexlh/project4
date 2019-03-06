8a014b2670dcbb457a5a9da785d77334d5067efe
littlexlh <507143878@qq.com>
2019-03-06 17:56:29 +0800

    2019-03-06

---
+++ basic_parent/basic_util/src/main/java/xlh/mall/project4/constant/GlobelConstants.java
@@ -0,0 +1,7 @@
+package xlh.mall.project4.constant;
+
+//定义全局常量【这个项目中只要依赖了basic_util的模块就都可以使用】
+public class GlobelConstants {
+    //treeData的redis的key
+    public static final String REDIS_PRODUCTTYPE_KEY = "treeData";
+}
--- zuul_server_9527/src/main/java/xlh/mall/project4/swagger/DocumentationConfig.java
+++ zuul_server_9527/src/main/java/xlh/mall/project4/swagger/DocumentationConfig.java
@@ -17,7 +17,7 @@ public class DocumentationConfig implements SwaggerResourcesProvider {
         //aigou网关前缀,employee网关路径   ；swagger的路径 ，以后增加了接口就在这配置就ok
         resources.add(swaggerResource("用户系统", "/xlh/employee/v2/api-docs", "2.0"));
         resources.add(swaggerResource("商品系统", "/xlh/product/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("基础系统", "/xlh/common/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("公共系统", "/xlh/common/v2/api-docs", "2.0"));
         return resources;
     }
 
