d53d9f6bba5e0c27707a329458d1641c3d36ab9e
littlexlh <507143878@qq.com>
2019-03-08 00:14:20 +0800

    2019-03-08

--- basic_parent/basic_util/src/main/java/xlh/mall/project4/constant/GlobelConstants.java
+++ basic_parent/basic_util/src/main/java/xlh/mall/project4/constant/GlobelConstants.java
@@ -2,6 +2,16 @@ package xlh.mall.project4.constant;
 
 //定义全局常量【这个项目中只要依赖了basic_util的模块就都可以使用】
 public class GlobelConstants {
+
     //treeData的redis的key
     public static final String REDIS_PRODUCTTYPE_KEY = "treeData";
+
+    //模板数据:model的key
+    public static final String PAGE_MODE="page_mode";
+
+    //哪一个模板
+    public static final String PAGE_TEMPLATE="page_template";
+
+    //最终输出到哪一个html
+    public static final String PAGE_TEMPLATE_HTML="page_template_html";
 }
